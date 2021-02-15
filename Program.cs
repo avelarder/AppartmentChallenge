474eb41696e8362acfda95239edbdaeaf8145c96
Arturo Velarde <arturovr82@hotmail.com>
2021-02-15 09:30:38 -0300

    Initial changes

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppartmentChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] req = { "gym", "school", "store" };
+
+            BlockItem[] blocks = {
+                new BlockItem { gym = false, school = true, store = false },
+                new BlockItem { gym = true, school = false, store = false },
+                new BlockItem { gym = true, school = true, store = false },
+                new BlockItem { gym = false, school = true, store = false },
+                new BlockItem { gym = false, school = true, store = true },
+            };
+
+            //Your Code here...
+        }
+    }
+
+
+
+    public struct BlockItem
+    {
+        public bool gym;
+        public bool school;
+        public bool store;
+    }
+}
+
---
+++ Readme.md
@@ -0,0 +1,10 @@
+*Instructions:
+Find the shortest distance to meet the requirements.
+For a given set of street blocks, let's find the block to rent an appartment that allows to walk the shortest distance for specific requirements, for instance: Gym, School, Bus Station are example of requirements. Not all blocks might have all the requirements.
+
+See "AppartmentChallenge.PNG"
+
+The code has a list of requirements and a list of blocks, find the index corresponding to the block for resting an appartment.
+
+*Video Source:
+https://www.youtube.com/watch?v=rw4s4M3hFfs&t=2458s
